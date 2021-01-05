e0092447ef7ba57bdb6efed3f14cce62f4787dec
Paulo Cândido <paulo.candido@g.globo>
2021-01-05 08:42:32 -0300

    chore: configure module-alias

---
+++ src/util/module-alias.ts
@@ -0,0 +1,9 @@
+import path from 'path'
+import moduleAlias from 'module-alias'
+
+const basePath = path.resolve(__dirname, '..', '..')
+
+moduleAlias.addAliases({
+    '@src': path.join(basePath, 'src'),
+    '@test': path.join(basePath, 'test')
+})
